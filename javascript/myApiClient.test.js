d4729ef58405789ef1f619f90f8a8182055b4b0d
Luís Soares <lsoares@newstore.com>
2023-12-31 01:46:29 +0000

    simplify js

--- javascript/myApiClient.test.js
+++ javascript/myApiClient.test.js
@@ -2,38 +2,38 @@ import express from "express";
 import { afterEach, describe, expect, it } from "vitest";
 import bodyParser from "body-parser";
 
-describe("Test Server", () => {
-  let testServer;
-  afterEach(() => testServer.close());
+// run tests with: npm t
+let testServer;
+afterEach(() => testServer.close());
 
-  it("executes query", async () => {
+it("executes query", async () => {
+  testServer = express()
+    .get("/someResource/id123", (_, res) => {
+      res.send("Hello, mock server!");
+    })
+    .listen(3000);
+  const apiClient = new MyApiClient("http://localhost:3000");
+
+  const something = await apiClient.getSomething("id123");
+
+  expect(something).toBe("Hello, mock server!");
+});
+
+it("executes command", () =>
+  new Promise(async (done) => {
     testServer = express()
-      .get("/someResource/id123", (_, res) => {
-        res.send("Hello, mock server!");
+      .use(bodyParser.text())
+      .post("/someResource", async (req, _) => {
+        expect(req.body).toBe("some data");
+        done();
       })
       .listen(3000);
     const apiClient = new MyApiClient("http://localhost:3000");
 
-    const something = await apiClient.getSomething("id123");
+    await apiClient.postSomething("some data");
+  }));
 
-    expect(something).toBe("Hello, mock server!");
-  });
-
-  it("executes command", () =>
-    new Promise(async (done) => {
-      testServer = express()
-        .use(bodyParser.text())
-        .post("/someResource", async (req, _) => {
-          expect(req.body).toBe("some data");
-          done();
-        })
-        .listen(3000);
-      const apiClient = new MyApiClient("http://localhost:3000");
-
-      await apiClient.postSomething("some data");
-    }));
-});
-
+// implementation:
 class MyApiClient {
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
