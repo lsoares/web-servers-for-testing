9e24a331f7e2203ba09cb1b7d66ef002c6016b4b
Luís Soares <lsoares@newstore.com>
2024-01-03 23:27:01 +0000

    add ruby example

--- ruby/spec/my_api_client_spec.rb
+++ ruby/spec/my_api_client_spec.rb
@@ -1,26 +1,39 @@
-require 'sinatra'
 require 'net/http'
+require 'webrick'
 
-# run tests with: bundle exec rspec
 describe 'my api client tests' do
+  let(:test_server) { WEBrick::HTTPServer.new(Port: 8000) }
+  after(:each) do
+    test_server.shutdown
+    @server_thread.join if @server_thread
+  end
+
   it 'performs a query' do
-    class TestServer < Sinatra::Base
-      get '/someResource/id123' do
-        'Hello, World!'
-      end
+    test_server.mount_proc '/someResource' do |req, res|
+      res.body = 'Hello, mock server!'
     end
-    @server_thread = Thread.new { run TestServer }
-    # TestServer.run!
-    api_client = MyApiClient.new('http://127.0.0.1:4567')
+    @server_thread = Thread.new { test_server.start }
+    api_client = MyApiClient.new("http://localhost:#{test_server.config[:Port]}")
 
-    something = api_client.get_something("id123")
+    something = api_client.get_something('id_123')
 
-    expect(something).to be "Hello, World!"
-    TestServer.quit!
+    expect(something).to eq('Hello, mock server!')
+  end
+
+  it 'performs a command' do
+    posted_data = ''
+    test_server.mount_proc '/someResource' do |req, res|
+      posted_data = req.body
+    end
+    @server_thread = Thread.new { test_server.start }
+    api_client = MyApiClient.new("http://localhost:#{test_server.config[:Port]}")
+
+    api_client.post_something('some data')
+
+    expect(posted_data).to eq('some data')
   end
 end
 
-# implementation:
 class MyApiClient
   attr_reader :base_url
 
@@ -35,7 +48,6 @@ class MyApiClient
   end
 
   def post_something(data)
-    response = @client.post('/someResource', data.to_json, headers)
-    response.code
+    @client.post('/someResource', data)
   end
 end
