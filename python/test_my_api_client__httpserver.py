2daa46edb1061268256bc62e88a158d797d3fa01
Luís Soares <lsoares@newstore.com>
2024-01-04 09:32:49 +0000

    simplify python ex.

--- python/test_my_api_client__httpserver.py
+++ python/test_my_api_client__httpserver.py
@@ -7,14 +7,14 @@ from test_my_api_client__httpserverlib import MyApiClient
 
 
 # to run tests: poetry run pytest
-def test_performs_query(server: HTTPServer):
+def test_performs_query(test_server: HTTPServer):
     class MyHandler(BaseHTTPRequestHandler):
         def do_GET(self):
             assert self.path == '/someResource/id123'
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b'Hello, mock server!')
-    server.RequestHandlerClass = MyHandler
+    test_server.RequestHandlerClass = MyHandler
     api_client = MyApiClient('http://localhost:8000')
 
     something = api_client.get_something('id123')
@@ -22,17 +22,16 @@ def test_performs_query(server: HTTPServer):
     assert something == 'Hello, mock server!'
 
 
-def test_performs_command(server: HTTPServer):
+def test_performs_command(test_server: HTTPServer):
     posted_data = ''
-    class PerformsCommandHandler(BaseHTTPRequestHandler):
+    class MyHandler(BaseHTTPRequestHandler):
         def do_POST(self):
             assert self.path == '/someResource'
             nonlocal posted_data
             posted_data = self.rfile.read(int(self.headers['Content-Length'])).decode('utf-8')
-            self.send_response(200)
+            self.send_response(201)
             self.end_headers()
-            self.wfile.write(b'')
-    server.RequestHandlerClass = PerformsCommandHandler
+    test_server.RequestHandlerClass = MyHandler
     api_client = MyApiClient('http://localhost:8000')
 
     api_client.post_something('some data')
@@ -40,14 +39,12 @@ def test_performs_command(server: HTTPServer):
     assert posted_data == 'some data'
 
 
-@pytest.fixture
-def server():
+@pytest.fixture(scope='session')
+def test_server():
     server = HTTPServer(('localhost', 8000), None)
     server_thread = Thread(target=server.serve_forever)
     server_thread.start()
-
     yield server
-
     server.shutdown()
     server.server_close()
     server_thread.join()
